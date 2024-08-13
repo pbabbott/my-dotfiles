9bca360bb980a2d8953a216abce75339aa92b446
Brandon Abbott <pbabbott@gmail.com>
2024-08-13 10:25:11 -0500

    Install oh my zsh plugins in install script

--- install.sh
+++ install.sh
@@ -5,4 +5,17 @@ sudo apt-get install stow
 sudo apt-get install fastfetch
 sudo apt-get install tmux
 
-stow .
+# Install zsh plugins
+install_ohmyzh_plugin() {
+    local GIT_REPO="$1"
+    local PLUGIN_NAME="$2"
+    local TARGET_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$PLUGIN_NAME"
+    echo "Target Dir is $TARGET_DIR"
+    if [ ! -d $TARGET_DIR ]; then
+        echo git clone $GIT_REPO $TARGET_DIR
+    fi
+}
+
+install_ohmyzh_plugin https://github.com/zsh-users/zsh-autosuggestions zsh-autosuggestions
+install_ohmyzh_plugin https://github.com/MichaelAquilina/zsh-you-should-use.git zsh-you-should-use
+install_ohmyzh_plugin https://github.com/superbrothers/zsh-kubectl-prompt.git zsh-kubectl-prompt
